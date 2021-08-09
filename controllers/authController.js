cfeb70190939062618362b72ed3163c552b24555
miglekuba <migle.kubiliute@gmail.com>
2021-08-09 18:09:03 +0100

    Add signup functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User')
+const jwt = require('jsonwebtoken')
 //handle errs
 const handleErrors = (err) => {
     console.log(err.message, err.code)
@@ -10,13 +11,21 @@ const handleErrors = (err) => {
         return errors;
     }
 
-    //validation errprs
+    //validation errors
     if (err.message.includes('user validation failed')) {
         Object.values(err.errors).forEach(({ properties }) => {
             errors[properties.path] = properties.message
         });
     }
     return errors;
+}
+
+const maxAge = 3 * 24 * 60 * 60;
+
+const createToken = (id) => {
+    return jwt.sign({ id }, 'secret', {
+        expiresIn: maxAge
+    });
 }
 
 module.exports.signup_get = (req, res) => {
@@ -29,9 +38,13 @@ module.exports.login_get = (req, res) => {
 
 module.exports.signup_post = async (req, res) => {
     const { email, password } = req.body;
+
     try {
         const user = await User.create({ email, password })
-        res.status(201).json(user);
+        const token = createToken(user._id)
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 })
+        res.status(201).json({user: user._id});
+
     } catch (err) {
         const errors = handleErrors(err)
         res.status(400).json({ errors })
